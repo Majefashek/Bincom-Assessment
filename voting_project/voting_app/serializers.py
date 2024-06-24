191730787d15a0c7a559c3bdd1ed0d4adf9c29a7
Majefashek <abdullahishuaibumaje@gmail.com>
2024-06-24 01:45:46 +0100

    merge conflict resolved

--- voting_project/voting_app/serializers.py
+++ voting_project/voting_app/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 from .models import PollingUnit, AnnouncedPuResults,Lga
 
+class MyPollingUnitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollingUnit
+        fields = ['id','polling_unit_id', 'ward_id', 'lga_id', 'uniquewardid', 'polling_unit_number', 'polling_unit_name', 'polling_unit_description', 'lat', 'long', 'entered_by_user', 'user_ip_address']
+
+
+class AnnouncedPuResultsSerializerFormCreation(serializers.Serializer):
+    polling_unit_uniqueid = serializers.CharField(max_length=50)
+    results = serializers.ListField(
+        child=serializers.DictField(
+            child=serializers.IntegerField()
+        )
+    )
+    entered_by_user = serializers.CharField(max_length=50)
+    user_ip_address = serializers.CharField(max_length=50)
+
 class LgaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Lga
--- voting_project/voting_app/urls.py
+++ voting_project/voting_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('post_new_polling_unit_result/',views.NewPollingUnitPostResults.as_view(),name='post_new_poll_result'),
     path('local_governments/results/<str:lga_id>', views.LocalGovernmentResult.as_view(), name='local_government_results'),
     path('local_governments/', views.LocalGovernmentListAPIView.as_view(), name='local_governments'),
     path('polling_units/', views.PollingUnitListAPIView.as_view(), name='polling_units'),
