d8e8983010b9726f91ed4845da07c82b7f751926
Majefashek <abdullahishuaibumaje@gmail.com>
2024-06-24 01:44:00 +0100

    implemented the post new polling unit result

--- voting_project/voting_app/views.py
+++ voting_project/voting_app/views.py
@@ -4,95 +4,89 @@ from .models import PollingUnit, AnnouncedPuResults,Lga,AnnouncedLgaResults
 from .serializers import PollingUnitSerializer, AnnouncedPuResultsSerializer,LgaSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import datetime
+from datetime import datetime
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from django.utils import timezone
 
 
 
 
 class NewPollingUnitPostResults(APIView):
-    #swagger form 
     @swagger_auto_schema(
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'polling_unit_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Polling unit ID'),
-            'ward_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Ward ID'),
-            'lga_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Local Government Area ID'),
-            'uniquewardid': openapi.Schema(type=openapi.TYPE_INTEGER, description='Unique Ward ID'),
-            'polling_unit_number': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit number'),
-            'polling_unit_name': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit name'),
-            'polling_unit_description': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit description'),
-            'lat': openapi.Schema(type=openapi.TYPE_STRING, description='Latitude'),
-            'long': openapi.Schema(type=openapi.TYPE_STRING, description='Longitude'),
-            'entered_by_user': openapi.Schema(type=openapi.TYPE_STRING, description='Entered by user'),
-            'user_ip_address': openapi.Schema(type=openapi.TYPE_STRING, description='User IP address'),
-            'results': openapi.Schema(
-                type=openapi.TYPE_ARRAY,
-                items=openapi.Schema(
-                    type=openapi.TYPE_OBJECT,
-                    properties={
-                        'party_abbreviation': openapi.Schema(type=openapi.TYPE_STRING, description='Party abbreviation'),
-                        'party_score': openapi.Schema(type=openapi.TYPE_INTEGER, description='Party score')
-                    }
-             )
-            )
-        }
-    ),
-) 
-        
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'ward_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Ward ID'),
+                'lga_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Local Government Area ID'),
+                'uniquewardid': openapi.Schema(type=openapi.TYPE_INTEGER, description='Unique Ward ID'),
+                'polling_unit_number': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit number'),
+                'polling_unit_name': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit name'),
+                'polling_unit_description': openapi.Schema(type=openapi.TYPE_STRING, description='Polling unit description'),
+                'lat': openapi.Schema(type=openapi.TYPE_STRING, description='Latitude'),
+                'long': openapi.Schema(type=openapi.TYPE_STRING, description='Longitude'),
+                'entered_by_user': openapi.Schema(type=openapi.TYPE_STRING, description='Entered by user'),
+                'user_ip_address': openapi.Schema(type=openapi.TYPE_STRING, description='User IP address'),
+                'results': openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        properties={
+                            'party_abbreviation': openapi.Schema(type=openapi.TYPE_STRING, description='Party abbreviation'),
+                            'party_score': openapi.Schema(type=openapi.TYPE_INTEGER, description='Party score')
+                        }
+                    )
+                )
+            }
+        ),
+        responses={201: 'Success', 400: 'Bad Request'}
+    )
     def post(self, request):
+        try:
         # Step 1: Create the new polling unit
-        polling_unit_data = {
-            'polling_unit_id': request.data.get('polling_unit_id'),
-            'ward_id': request.data.get('ward_id'),
-            'lga_id': request.data.get('lga_id'),
-            'uniquewardid': request.data.get('uniquewardid'),
-            'polling_unit_number': request.data.get('polling_unit_number'),
-            'polling_unit_name': request.data.get('polling_unit_name'),
-            'polling_unit_description': request.data.get('polling_unit_description'),
-            'lat': request.data.get('lat'),
-            'long': request.data.get('long'),
-            'entered_by_user': request.data.get('entered_by_user'),
-            'user_ip_address': request.data.get('user_ip_address')
-        }
-        polling_unit_serializer = PollingUnitSerializer(data=polling_unit_data)
-        
-        if polling_unit_serializer.is_valid():
-            polling_unit = polling_unit_serializer.save()
-            
-            # Step 2: Store the results using the newly created polling unit's ID
-            results_serializer = AnnouncedPuResultsSerializer(data=request.data)
-            
-            if results_serializer.is_valid():
-                results = results_serializer.validated_data['results']
-                entered_by_user = results_serializer.validated_data['entered_by_user']
-                user_ip_address = results_serializer.validated_data['user_ip_address']
-                date_entered = datetime.now()
-                
+            polling_unit_data = {
+                'polling_unit_id':1,
+                'ward_id': request.data.get('ward_id'),
+                'lga_id': request.data.get('lga_id'),
+                'uniquewardid': request.data.get('uniquewardid'),
+                'polling_unit_number': request.data.get('polling_unit_number'),
+                'polling_unit_name': request.data.get('polling_unit_name'),
+                'polling_unit_description': request.data.get('polling_unit_description'),
+                'lat': request.data.get('lat'),
+                'long': request.data.get('long'),
+                'entered_by_user': request.data.get('entered_by_user'),
+                'user_ip_address': request.data.get('user_ip_address')
+            }
+            polling_unit_serializer = PollingUnitSerializer(data=polling_unit_data)
+
+            if polling_unit_serializer.is_valid():
+                polling_unit = polling_unit_serializer.save()
+                # Update the polling unit instance to set the polling_unit_id
+                polling_unit.polling_unit_id = polling_unit.uniqueid
+                polling_unit.save()
+
+                # Step 2: Store the results using the newly created polling unit's ID
+                results = request.data.get('results', [])
+                entered_by_user = request.data.get('entered_by_user')
+                user_ip_address = request.data.get('user_ip_address')
+                date_entered =datetime.now()
+
                 # Save results for each party
                 for result in results:
-                    for party_abbreviation, party_score in result.items():
-                        AnnouncedPuResults.objects.create(
-                            polling_unit_uniqueid=polling_unit.uniqueid,
-                            party_abbreviation=party_abbreviation,
-                            party_score=party_score,
-                            entered_by_user=entered_by_user,
-                            date_entered=date_entered,
-                            user_ip_address=user_ip_address
-                        )
-                
+                    AnnouncedPuResults.objects.create(
+                        polling_unit_uniqueid=polling_unit.uniqueid,
+                        party_abbreviation=result['party_abbreviation'],
+                        party_score=result['party_score'],
+                        entered_by_user=entered_by_user,
+                        date_entered=date_entered,
+                        user_ip_address=user_ip_address
+                    )
+
                 return Response({'status': 'success', 'message': 'Polling unit and results saved successfully'}, status=200)
-            
-            return Response(results_serializer.errors, status=400)
-        
-        return Response(polling_unit_serializer.errors, status=200)
 
-
-
-
-
+            return Response(polling_unit_serializer.errors, status=400)
+        except Exception as e:
+            return Response({'error': str(e)}, status=500)
 
 class LocalGovernmentResult(APIView):
     '''
