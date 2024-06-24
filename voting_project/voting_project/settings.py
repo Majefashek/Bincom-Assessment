4e83a224a5049b88a26229e60d1fb258aec4586b
Majefashek <abdullahishuaibumaje@gmail.com>
2024-06-24 02:14:52 +0100

    configured the django app for hosting

--- voting_project/voting_project/settings.py
+++ voting_project/voting_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-ij_&fd#&lkwjxr9u%o6l)yxg_(9i7*f0x$6u&y41xjcbwq3z#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
